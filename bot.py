755d9142e082779357c3053bef80f4f868b50530
ABD-01 <myac931@gmail.com>
2020-10-24 20:10:46 +0530

    Getting the servername from bot

--- bot.py
+++ bot.py
@@ -1,9 +1,18 @@
 import discord
 TOKEN="NzY5NTYwMzI1Mzg0OTYyMDc5.X5Qy5w.Hwv0eN5jwCJUFxt8rgAGm7EEEk0"
+GUILD="Second Years-IvLabs"
+
 client = discord.Client()
- 
+
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
 
 client.run(TOKEN)
