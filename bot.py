42ff475fd30349134103695b227c4dee8a64d4ce
ABD-01 <myac931@gmail.com>
2020-10-25 16:27:50 +0530

    Added to respond to messages

--- bot.py
+++ bot.py
@@ -4,15 +4,46 @@ GUILD="Second Years-IvLabs"
 
 client = discord.Client()
 
-@client.event
-async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+class CustomClient(discord.Client):
+    async def on_ready(self):
+        for guild in client.guilds:
+            if guild.name == GUILD:
+                break
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+        print(
+            f'{self.user} is connected to the following guild:\n'
+            f'{guild.name}(id: {guild.id})\n'
+        )
 
+        members = '\n - '.join([member.name for member in guild.members])
+        print(f'Guild Members:\n - {members}')
+
+    async def on_message(self,message):
+        if message.content.startswith('$greet'):
+            channel = message.channel
+            await channel.send('Say hello!')
+
+            def check(m):
+                return m.content == 'hello' and m.channel == channel
+
+            msg = await self.wait_for('message', check=check)
+            await channel.send('Hello {.author}!'.format(msg))
+
+        elif message.content.startswith('$thumb'):
+            channel = message.channel
+            await channel.send('Send me that 👍 reaction, mate')
+
+            def check(reaction, user):
+                return user == message.author and str(reaction.emoji) == '👍'
+
+            try:
+                reaction, user = await client.wait_for('reaction_add', timeout=60.0, check=check)
+            except asyncio.TimeoutError:
+                await channel.send('👎')
+            else:
+                await channel.send('👍')
+
+
+client = CustomClient()
 client.run(TOKEN)
+
